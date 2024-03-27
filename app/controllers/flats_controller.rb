eeae16f9e68878eee368f3d5050767fd65efe9e9
Olivier Perrin <olivier.perrin@ieseg.fr>
2024-03-27 11:52:30 +0100

    flatcontroller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,21 @@
 class FlatsController < ApplicationController
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.user = current_user
+
+    if @flat.save
+      redirect_to root_path
+    else
+
+    end
+
+  end
+
+  def flat_params
+    params.require(:flat).permit(:title, :address, :city, :country, :price_per_night, :description, photos: [])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
+  # routes for flats
+  resources :flats, only: [:new, :create, :show]
 end
